278da98fd5da3e758c2d6fde92905bb6376a1bcd
cvv25 <carlosbelles7@gmail.com>
2023-01-09 15:44:28 +0100

    Initial commit

---
+++ src/main/java/com/example/producto1/Producto1Application.java
@@ -0,0 +1,13 @@
+package com.example.producto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Producto1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Producto1Application.class, args);
+    }
+
+}
